f0f723784539592656e1b616cbd833c23d871872
Dmytro Osiichuk <dmytro.osiychuk@gmail.com>
2021-09-29 16:21:08 +0200

    Add move identification logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-let computerMove = `rock`;
-let playerMove = `paper`;
+//Establish computer's move
+let computerMove;
+let randMove = Math.floor(Math.random() * 3 + 1);
+if (randMove == 1) {
+  computerMove = "Rock";
+} else if (randMove == 2) {
+  computerMove = "Paper";
+} else {
+  computerMove = "Scissors";
+}
+//Establish player's move
+let playerInput = prompt("Choose your move: 1: Rock; 2: Paper; 3: Scissors");
 
-printMessage('I played ' + computerMove + '! If you move is paper, you win!');
+let playerMove;
+if (playerInput == "1") {
+  playerMove = "Rock";
+} else if (playerInput == "2") {
+  playerMove = "Paper";
+} else {
+  playerMove = "Scissors";
+}
+console.log(`Player move: ${playerMove}`);
+console.log(`Computer move: ${computerMove}`);
