cd1429a28a7a5331707815834cea56e0788e88bb
Dmytro Osiichuk <dmytro.osiychuk@gmail.com>
2021-09-29 16:43:57 +0200

    Add logic to identify the winer

--- js/script.js
+++ js/script.js
@@ -16,8 +16,26 @@ if (playerInput == "1") {
   playerMove = "Rock";
 } else if (playerInput == "2") {
   playerMove = "Paper";
-} else {
+} else if (playerInput == "3") {
   playerMove = "Scissors";
+} else {
+  playerMove = "Unknown";
 }
 console.log(`Player move: ${playerMove}`);
 console.log(`Computer move: ${computerMove}`);
+let winner;
+//check the winner
+if (computerMove == playerMove) {
+  winner = "Draw";
+} else if (
+  (computerMove == "Rock" && playerMove == "Paper") ||
+  (computerMove == "Scissors" && playerMove == "Paper") ||
+  (computerMove == "Rock" && playerMove == "Scissors")
+) {
+  winner = "Computer";
+} else if ((playerMove = "Unknown")) {
+  winner = "Winner cannot be established";
+} else {
+  winner = "Player";
+}
+console.log(`The winner is: ${winner}`);
