5008d648d16a91c12424d086a12519876159b5e9
Dmytro Osiichuk <dmytro.osiychuk@gmail.com>
2021-09-29 17:37:06 +0200

    Add button-based game interface, fix bug (paper beats rock)

--- js/script.js
+++ js/script.js
@@ -1,70 +1,59 @@
-//function getMoveName
-function getMoveName(moveId) {
-  if (moveId == 1) {
-    return "Rock";
-  } else if (moveId == 2) {
-    return "Paper";
-  } else if (moveId == 3) {
-    return "Scissors";
-  } else {
-    return "Unknown";
+function playGame(playerInput) {
+  clearMessages();
+  //function getMoveName
+  function getMoveName(moveId) {
+    if (moveId == 1) {
+      return "Rock";
+    } else if (moveId == 2) {
+      return "Paper";
+    } else if (moveId == 3) {
+      return "Scissors";
+    } else {
+      return "Unknown";
+    }
   }
-}
-//function displayResults
-function displayResult(playerMove, computerMove) {
-  if (computerMove == playerMove) {
-    return "Draw";
-  } else if (
-    (computerMove == "Rock" && playerMove == "Paper") ||
-    (computerMove == "Scissors" && playerMove == "Paper") ||
-    (computerMove == "Rock" && playerMove == "Scissors")
-  ) {
-    return "Computer";
-  } else if (playerMove == "Unknown") {
-    return "Winner cannot be established";
-  } else {
-    return "Player";
+  //function displayResults
+  function displayResult(playerMove, computerMove) {
+    if (computerMove == playerMove) {
+      return "Draw";
+    } else if (
+      (computerMove == "Paper" && playerMove == "Rock") ||
+      (computerMove == "Scissors" && playerMove == "Paper") ||
+      (computerMove == "Rock" && playerMove == "Scissors")
+    ) {
+      return "Computer";
+    } else if (playerMove == "Unknown") {
+      return "Winner cannot be established";
+    } else {
+      return "Player";
+    }
   }
+
+  //Establish computer's move
+
+  let randMove = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randMove);
+
+  //Establish player's move
+  //let playerInput = prompt("Choose your move: 1: Rock; 2: Paper; 3: Scissors");
+  let playerMove = getMoveName(parseInt(playerInput));
+
+  printMessage(`Player move: ${playerMove}`);
+  printMessage(`Computer move: ${computerMove}`);
+  let winner = displayResult(playerMove, computerMove);
+
+  printMessage(`The winner is: ${winner}`);
 }
 
-//Establish computer's move
-
-let randMove = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randMove);
-
-//Establish player's move
-let playerInput = prompt("Choose your move: 1: Rock; 2: Paper; 3: Scissors");
-let playerMove = getMoveName(parseInt(playerInput));
-
-console.log(`Player move: ${playerMove}`);
-console.log(`Computer move: ${computerMove}`);
-let winner = displayResult(playerMove, computerMove);
-
-console.log(`The winner is: ${winner}`);
-
-//code to be deleted
-//identify computer move
-// if (playerInput == "1") {
-//   playerMove = "Rock";
-// } else if (playerInput == "2") {
-//   playerMove = "Paper";
-// } else if (playerInput == "3") {
-//   playerMove = "Scissors";
-// } else {
-//   playerMove = "Unknown";
-// }
-
-//check the winner
-// if (computerMove == playerMove) {
-//   winner = "Draw";
-// } else if (
-//   (computerMove == "Rock" && playerMove == "Paper") ||
-//   (computerMove == "Scissors" && playerMove == "Paper") ||
-//   (computerMove == "Rock" && playerMove == "Scissors")
-// ) {
-//   winner = "Computer";
-// } else if ((playerMove = "Unknown")) {
-//   winner = "Winner cannot be established";
-// } else {
-//   winner = "Player";
-// }
+document.getElementById("play-rock").addEventListener("click", function () {
+  clearMessages();
+  playGame(1);
+});
+document.getElementById("play-paper").addEventListener("click", function () {
+  clearMessages();
+  playGame(2);
+});
+document.getElementById("play-scissors").addEventListener("click", function () {
+  clearMessages();
+  playGame(3);
+});
